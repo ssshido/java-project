26689224690f20f708aea28efb6190fbc13fcf3d
Savva <hasanovsavva69@gmail.com>
2025-12-19 22:47:25 +0500

    Update CsvLoader.java

--- src/main/java/com/savva/service/CsvLoader.java
+++ src/main/java/com/savva/service/CsvLoader.java
@@ -1,6 +1,6 @@
-package com.bodya.service;
+package com.savva.service;
 
-import com.bodya.model.Billionaire;
+import com.savva.model.Billionaire;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
